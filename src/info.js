10281d9a9b5adf299b8e1ff6cb75abd5f5082f75
arclopes <alxcrv@gmail.com>
2023-03-13 18:23:13 -0300

    FEATURE: new info script

---
+++ src/info.js
@@ -0,0 +1,52 @@
+import { argParser } from "./utils/argParser";
+
+/** @param {import(".").NS} ns */
+function getMoneyInfo(ns, args) {
+    let target = args[0];
+    let moneyAvailable = ns.getServerMoneyAvailable(target);
+    let maxMoney = ns.getServerMaxMoney(target);
+    let ratio = moneyAvailable / maxMoney;
+
+    ns.tprint(`Money available: ${moneyAvailable}`);
+    ns.tprint(`Max money: ${maxMoney}`);
+    ns.tprint(`Ratio: ${ratio}`);
+}
+
+/** @param {import(".").NS} ns */
+function getServerInfo(ns, args) {
+    let target = args[0]; 
+    let minSecurity = ns.getServerMinSecurityLevel(target);
+    let currentSecurity = ns.getServerSecurityLevel(target);
+    let hackingLevel = ns.getServerRequiredHackingLevel(target);
+    let memory = ns.getServerMaxRam(target);
+    let ports = ns.getServerNumPortsRequired(target);
+
+    ns.tprint(`Min security: ${minSecurity}`);
+    ns.tprint(`Current security: ${currentSecurity}`);
+    ns.tprint(`Required hacking level: ${hackingLevel}`);
+    ns.tprint(`Memory: ${memory}`);
+    ns.tprint(`Ports: ${ports}`);
+}
+
+export function autocomplete(data) {
+    return [...data.servers, ...data.scripts,]
+}
+
+/** @param {import(".").NS} ns */
+export async function main(ns) {
+    const argsFn = {
+        'money': getMoneyInfo,
+        'server': getServerInfo
+    }
+
+    let args = argParser(ns.args);
+    if (args) {
+        for (let key in args) {
+            if (argsFn[key]) { argsFn[key](ns, args[key]) }
+        }
+    }
+    else {
+        ns.tprint("No arguments provided");
+        ns.tprint("Usage: info.js --[server|hacking] <server>");
+    }
+}
