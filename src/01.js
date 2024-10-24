81b1033d668787717df5c806a3b7a5e965be9746
Alex Lopes <alxcrv@gmail.com>
2024-10-24 16:23:56 -0300

    FIX: delay execution of ahckerSpider

--- src/01.js
+++ src/01.js
@@ -3,10 +3,10 @@ export async function main(ns) {
     ns.tprint("Running initial scripts");
     ns.exec('crackerSpider.js', 'home', 1);
     await ns.sleep(10);
-    ns.exec('hackerSpider.js', 'home', 1);
-    await ns.sleep(10);
     ns.exec('purchaseServers.js', 'home', 1);
     await ns.sleep(10);
     ns.exec('starters/buyTORRouter.js', 'home', 1);
+    await ns.sleep(1e4);
+    ns.exec('hackerSpider.js', 'home', 1);
     ns.tprint("Starter finished.")
 }
