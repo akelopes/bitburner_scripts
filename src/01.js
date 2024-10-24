173c850caf3828737bbd68fbe22bb1548c78b933
Alex Lopes <alxcrv@gmail.com>
2024-10-24 14:23:09 -0300

    FEAT: new startup script 01.js

--- src/01.js
+++ src/01.js
@@ -1,8 +1,12 @@
 /** @param {import(".").NS} ns */
 export async function main(ns) {
     ns.tprint("Running initial scripts");
-    ns.run('spiderCracker.js', 1);
-    ns.run('purchaseServer.js', 1);
-    ns.run('starter/buyTORRouter.js', 1);
+    ns.exec('crackerSpider.js', 'home', 1);
+    await ns.sleep(10);
+    ns.exec('hackerSpider.js', 'home', 1);
+    await ns.sleep(10);
+    ns.exec('purchaseServers.js', 'home', 1);
+    await ns.sleep(10);
+    ns.exec('starters/buyTORRouter.js', 'home', 1);
     ns.tprint("Starter finished.")
 }
